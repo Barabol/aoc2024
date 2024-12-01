cf376b8d75840e0fa65803485154321d95acbbb1
Szymon <105214028+Barabol@users.noreply.github.com>
2024-12-01 10:36:29 +0100

    a

--- day1/main.py
+++ day1/main.py
@@ -1,1 +1,18 @@
+file = open("./input.txt","r");
+nums1 = []
+nums2 = []
+for line in file:
+    line = line.split("   ")
+    nums1.append(line[0])
+    nums2.append(line[1])
+if len(nums1) != len(nums2):
+    print("he?")
+else:
+    nums1.sort()
+    nums2.sort()
+    sum=0
+    for x in range(nums1.__len__()):
+        a = abs(int(nums1[x])-int(nums2[x]))
+        sum+=a
+    print("wynik: ",sum)
 
---
+++ day1/main2.py
@@ -0,0 +1,35 @@
+file = open("./input.txt","r");
+nums1 = []
+nums2 = []
+for line in file:
+    line = line.split("   ")
+    nums1.append(line[0])
+    nums2.append(line[1])
+if len(nums1) != len(nums2):
+    print("he?")
+
+else:
+    nums1.sort()
+    nums2.sort()
+    max = 0;
+    if int(nums1[nums1.__len__()-1]) > int(nums2[nums2.__len__()-1]):
+        max = nums1[nums1.__len__()-1]
+    else:
+        max = nums2[nums2.__len__()-1]
+    max = int(max)
+    nums3 = [0]*max*2
+    nums4 = [0]*max*2
+    print(nums3.__len__())
+    for x in nums1:
+        x = int(x)
+        nums3[x]+=1
+    for x in nums2:
+        x = int(x)
+        nums4[x]+=1
+
+    sum=0
+    for x in range(nums3.__len__()):
+        sum+= x*nums3[x]*nums4[x]
+        print(x,nums3[x],nums4[x],x*nums3[x]*nums4[x])
+    print("Wynik: ",sum)
+

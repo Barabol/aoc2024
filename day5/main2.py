d107af8032942ac008ea727db3032cbbf7a0af7b
Szymon <105214028+Barabol@users.noreply.github.com>
2024-12-05 22:29:38 +0100

    jak ja nie mam pojęcia co robie

---
+++ day5/main2.py
@@ -0,0 +1,64 @@
+retw = []
+updates=[]
+def isIn(a):
+    if retw.__len__() == 0:
+       return -1
+    count = -1
+    for x in retw:
+        count+=1
+        if x[0] == a:
+            return count
+    return -1
+def red():
+    c = 0
+    for line in file:
+        if line == "\n":
+            c+=1
+        match(c):
+            case 0:
+                splitter = [int(line.split("|")[0]),int(line.split("|")[1][:-1])]
+                holder = isIn(splitter[0])
+                if holder == -1:
+                    retw.append(splitter)
+                else:
+                    retw[holder].append(splitter[1])
+            case 1:
+                c+=1
+            case 2:
+                index = updates.__len__();
+                updates.append([])
+                for x in line[:-1].split(","):
+                    updates[index].append(int(x))
+
+def indexOf(lista,a):
+    for x in range(len(lista)):
+        if lista[x] == a:
+            return x
+    return -1
+def check(lista,index):
+    holder = retw[isIn(lista[index])]
+    for l in range(len(lista)):
+        if indexOf(holder,lista[l]) > 0 and lista[l] != lista[index] and indexOf(lista,lista[l]) < index:
+            return False
+    return True 
+def isGut(a):
+    kek = True
+    for y in range(len(a)):
+        if kek==True and isIn(a[y]) != -1:
+            if check(a,y)==False:
+                kek = False
+    return kek
+def format_(lista):
+    print()
+file = open("input1.txt","r")
+red()
+gut = []
+kek = False
+for x in updates:
+    if isGut(x) == False:
+        gut.append(x)
+print("\n",gut)
+sum=0
+for x in gut:
+    sum+=(x[(len(x)//2)])
+print("wynik: ",sum)
